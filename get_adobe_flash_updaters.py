d6edb00b1b845cdca7ccb2730738471ee894074a
Stephen Genusa <github@genusa.com>
2015-03-07 11:28:21 -0600

    Cleaned up code
    PEP8 cleanup

--- get_adobe_flash_updaters.py
+++ get_adobe_flash_updaters.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
-# Update to the latest Adobe Flash players
-# Limited to Windows right now though easy to change
+""" 
+Downloads the latest 2 Adobe Flash players for Windows,
+the Active-X and plugin Flash players, and then does a 
+no-prompt install of each of them.
+This can be changed to support another O/S by a simple
+change to the code
+"""
 
 import os
 import sys
@@ -8,46 +13,68 @@ import urllib2
 import subprocess
 from BeautifulSoup import BeautifulSoup
 
-if len(sys.argv) == 2 and sys.argv[1] == '-u':
-    url_opener = urllib2.urlopen('http://www.some_domain.com/flashu/g_flash.zip')
-    data = url_opener.read()
-    open('g_flash.zip', "wb").write(data)
-    print 'Downloaded g_flash.zip'
-    sys.exit()
+ADOBE_FLASH_URL = 'http://www.adobe.com/products/flashplayer/distribution3.html'
 
-url = 'http://www.adobe.com/products/flashplayer/distribution3.html'
 
-conn = urllib2.urlopen(url)
-html = conn.read()
+def check_for_updated_downloader():
+    """If the -u parameter is given the program will attempt to download
+       a zip file at the given URL allowing the program to self-update
+    """
+    if len(sys.argv) == 2 and sys.argv[1] == '-u':
+        url_opener = urllib2.urlopen('http://www.some_domain.com/flashu/g_flash.zip')
+        data = url_opener.read()
+        open('g_flash.zip', "wb").write(data)
+        print 'Downloaded latest g_flash.zip'
+        sys.exit()
 
-soup = BeautifulSoup(html)
-links = soup.findAll('a')
 
-download_counter = 0
-for tag in links:
-    link = tag.get('href',None)
-    if link != None and link.find('.msi') > -1 and link.find('/download') > -1:
-        try:
-            strfilename = link.rsplit('/', 1)[1]
-            if not os.path.exists(strfilename):
-                print "Attempting download of " + link
-                url_opener = urllib2.urlopen(link)
-                data = url_opener.read()
-                open(strfilename, "wb").write(data)
-                print 'Downloaded ' + link.rsplit('/', 1)[1]
-            else:
-                print strfilename, "already exists. skipping download."
-            subprocess.call("msiexec /quiet /passive /i " + strfilename)
-            download_counter += 1
-            if download_counter == 2:
-                print "Downloaded latest two .msi files..."
-                break
-        except urllib2.HTTPError, val:
-            if '404' in str(val): 
-                print "Couldn't download (404) " + url
-        except urllib2.URLError, val:
-            print "URL Error: " + url
-        except Exception, e:
-            print 'General error: ' + e.message
-            continue
+def main():
+    # Self-update check
+    check_for_updated_downloader
+    
+    # Get the Flash Distribution HTML
+    print "Downloading the Flash Player distribution information"
+    conn = urllib2.urlopen(ADOBE_FLASH_URL)
+    html = conn.read()
+    
+    # Parse the HTML with BeautifulSoup
+    print "Parsing the HTML"
+    soup = BeautifulSoup(html)
+    links = soup.findAll('a')
+    
+    download_counter = 0
+    for tag in links:
+        link = tag.get('href',None)
+        # Look for the .msi links
+        if link != None and link.find('.msi') > -1 and link.find('/download') > -1:
+            try:
+                player_filename = link.rsplit('/', 1)[1]
+                # Download the file if the file doesn't exist already
+                if not os.path.exists(player_filename):
+                    print "Attempting download of " + link
+                    url_opener = urllib2.urlopen(link)
+                    data = url_opener.read()
+                    open(player_filename, "wb").write(data)
+                    print 'Downloaded ' + link.rsplit('/', 1)[1]
+                else:
+                    print player_filename, "already exists. skipping download."
+                # Do a quiet install of the Flash player
+                print "Executing", player_filename
+                subprocess.call("msiexec /quiet /passive /i " + player_filename)
+                # Increment the download counter. We only want the first two
+                # listed: the (1) Active-X and (2) Plugin versions
+                download_counter += 1
+                if download_counter == 2:
+                    print "Downloaded latest two .msi files..."
+                    break
+            except urllib2.HTTPError, val:
+                if '404' in str(val): 
+                    print "Couldn't download (404) " + ADOBE_FLASH_URL
+            except urllib2.URLError, val:
+                print "URL Error: " + ADOBE_FLASH_URL
+            except Exception, e:
+                print 'General error: ' + e.message
+                continue
                 
+if __name__ == "__main__":
+    main()
