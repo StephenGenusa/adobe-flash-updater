6b2d71c679a529379e2a9d87f04bc75512b8b41b
Stephen Genusa <github@genusa.com>
2015-06-17 10:47:35 -0500

    Updated URLs and Version Checking
    Updated Adobe URLs. Now checks the installed version numbers against the Adobe Web site and installs if version numbers don't match the latest available on Adobe's web site. Skips the download and install if they match.

--- get_adobe_flash_updaters.py
+++ get_adobe_flash_updaters.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 """ 
 Downloads the latest 2 Adobe Flash players for Windows,
 the Active-X and plugin Flash players, and then does a 
@@ -14,6 +15,12 @@ April 30th 2015:
   Instead of using urllib2 for downloading the installation
   I am using the Python wget module so that there is a
   progress bar on the console for each download.
+
+June 17 2015:
+  Updated Adobe URLs. Now checks the installed version numbers against
+  the Adobe Web site and installs if version numbers don't match the
+  latest available on Adobe's web site. Skips the download and install
+  if they match.
 """
 
 # Standard Library
@@ -21,6 +28,8 @@ import os
 import sys
 import urllib2
 import subprocess
+from _winreg import *
+
 # 3rd Party
 from BeautifulSoup import BeautifulSoup
 import wget
@@ -42,40 +51,76 @@ def check_for_updated_downloader():
 def main():
     # Self-update check
     check_for_updated_downloader()
-    
+
+    # Get the installed versions
+    try:
+      aReg = ConnectRegistry(None,HKEY_LOCAL_MACHINE)
+      aKey = OpenKey(aReg, r"SOFTWARE\Macromedia\FlashPlayerActiveX")
+      activeXVersion = QueryValueEx(aKey, "Version")[0]
+      CloseKey(aKey)
+    except:
+      activeXVersion = ""
+
+    try:
+      aKey = OpenKey(aReg, r"SOFTWARE\Macromedia\FlashPlayerPlugin")
+      pluginVersion = QueryValueEx(aKey, "Version")[0]
+      CloseKey(aKey)
+    except:
+      pluginVersion = ""
+    CloseKey(aReg)  
+
+    print "=" * 60
+    print "Adobe Flash Players Installed:"
+    print "ActiveX Version is =", activeXVersion 
+    print "Plug-in Version is =", pluginVersion 
+    print "=" * 60
+   
     # Get the Flash Distribution HTML
-    print "Downloading the Flash Player distribution information"
+    print "Downloading the Current Flash Player Distribution Information"
     try:
         conn = urllib2.urlopen(ADOBE_FLASH_URL)
         html = conn.read()
         
         # Parse the HTML with BeautifulSoup
-        print "Parsing the HTML"
         soup = BeautifulSoup(html)
+
+        currentWebVersion = ""
+        for span in soup.findAll('span'):
+            if span.find("span","TextH3 LayoutSmallRow"):
+                currentWebVersion = span.getText().strip().split(' ')[2]
+                break
+
+        print "Current Version is =", currentWebVersion 
+        print "=" * 60
         links = soup.findAll('a')
-        
         download_counter = 0
         for tag in links:
             link = tag.get('href',None)
             # Look for the .msi links
-            if link != None and link.find('.msi') > -1 and link.find('/download') > -1:
+            if link != None and link.find('.msi') > -1 and link.find('/current/licensing/win/') > -1:
                 try:
+                    doDownload = False 
                     player_filename = link.rsplit('/', 1)[1]
+                    if player_filename.find("active_x") > -1 and activeXVersion != "" and activeXVersion != currentWebVersion:
+                      doDownload = True 
                     # Download the file if the file doesn't exist already
-                    if not os.path.exists(player_filename):
+                    if player_filename.find("plugin") > -1 and pluginVersion != "" and pluginVersion != currentWebVersion:
+                      doDownload = True 
+                    if doDownload and not os.path.exists(player_filename):
                         print "Attempting download of " + link
                         wget.download(link)
                         print '\nDownloaded ' + link.rsplit('/', 1)[1]
+                        # Do a quiet install of the Flash player
+                        print "Executing", player_filename
+                        subprocess.call("msiexec /quiet /passive /i " + player_filename)
                     else:
-                        print player_filename, "already exists. skipping download."
-                    # Do a quiet install of the Flash player
-                    print "Executing", player_filename
-                    subprocess.call("msiexec /quiet /passive /i " + player_filename)
+                        print player_filename, "already exists\n   or is already installed. Skipping."
                     # Increment the download counter. We only want the first two
                     # listed: the (1) Active-X and (2) Plugin versions
                     download_counter += 1
                     if download_counter == 2:
-                        print "Downloaded latest two .msi files..."
+                        print "=" * 60
+                        print "Done..."
                         break
                 except urllib2.HTTPError, val:
                     if '404' in str(val): 
